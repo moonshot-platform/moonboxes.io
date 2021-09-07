7bb8790c9ba176896b9936d79e39466367c84f04
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-09-08 00:04:11 +0300

    finished Landing Page's raise stakes section

--- src/app/components/moonbase/landing/raise-stakes/raise-stakes.component.ts
+++ src/app/components/moonbase/landing/raise-stakes/raise-stakes.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RaiseStakesComponent implements OnInit {
 
+  boxes: any[] = [
+    {
+      "img": "assets/media/images/moonbox/landing/wood_rgba0048.png",
+      "name": "Wood",
+      "quantity": "0,5B",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/silver_rgba0001.png",
+      "name": "Silver",
+      "quantity": "1B",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/gold_rgba0001.png",
+      "name": "Gold",
+      "quantity": "2B",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/diamond_rgba0034.png",
+      "name": "Diamond",
+      "quantity": "10B",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
+  scrollToElement(page: string, fragment: string): void {
+    const element = document.querySelector(`#${fragment}`)
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }
+
+
 
 }
