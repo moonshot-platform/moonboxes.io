5d43fef9fc0e0d04d2f1d46ec0f12a40d9d5cb67
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-09-10 11:27:41 +0300

    showing carousel name

--- src/app/components/moonbase/landing/limited/limited.component.ts
+++ src/app/components/moonbase/landing/limited/limited.component.ts
@@ -11,17 +11,47 @@ SwiperCore.use([EffectCoverflow]);
 })
 export class LimitedComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  slides: any[] = [
+    {
+      "img": "assets/media/images/moonbox/landing/322.jpg",
+      "name": "The Moonshooter #109",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/950.jpg",
+      "name": "The Moonshooter #110",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/951.jpg",
+      "name": "The Moonshooter #111",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/068.jpg",
+      "name": "The Moonshooter #112",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/372.jpg",
+      "name": "The Moonshooter #113",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/741.jpg",
+      "name": "The Moonshooter #114",
+    },
+    {
+      "img": "assets/media/images/moonbox/landing/575.jpg",
+      "name": "The Moonshooter #115",
+    },
+  ];
+
+  initialSlideStartIndex = Math.floor((this.slides.length - 1) / 2);
+  sliderIndex = this.initialSlideStartIndex;
+  currentSliderName = this.slides[this.sliderIndex].name;
 
   config: SwiperOptions = {
     slidesPerView: 3,
     effect: 'coverflow',
     direction: 'horizontal',
     centeredSlides: true,
-    //spaceBetween: 10,
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
@@ -33,7 +63,7 @@ export class LimitedComponent implements OnInit {
       sticky: true,
     },
     grabCursor: true,
-    initialSlide: 3,
+    initialSlide: this.initialSlideStartIndex,
     loop: true,
     coverflowEffect: {
       depth: 400,
@@ -41,41 +71,26 @@ export class LimitedComponent implements OnInit {
       slideShadows: false,
       rotate: 0,
       stretch: 0,
-    },
-
+    }
   };
+  constructor() {
+  }
+  ngOnInit(): void {
 
-  // Create array with 1000 slides
-  slides: any[] = [
-    {
-      "img": "assets/media/images/moonbox/landing/322.jpg",
-      "name": "",
-    },
-    {
-      "img": "assets/media/images/moonbox/landing/950.jpg",
-      "name": "",
-    },
-    {
-      "img": "assets/media/images/moonbox/landing/951.jpg",
-      "name": "",
-    },
-    {
-      "img": "assets/media/images/moonbox/landing/068.jpg",
-      "name": "",
-    },
-    {
-      "img": "assets/media/images/moonbox/landing/372.jpg",
-      "name": "",
-    },
-    {
-      "img": "assets/media/images/moonbox/landing/741.jpg",
-      "name": "",
-    },
-    {
-      "img": "assets/media/images/moonbox/landing/575.jpg",
-      "name": "",
-    },
-  ];
+  }
+
+
+  onSlideChange(index: any): void {
+    if (index.swipeDirection == "next") {
+      this.sliderIndex++;
+
+    } else if (index.swipeDirection == "prev") {
+      this.sliderIndex--;
+    }
+    this.sliderIndex = (this.sliderIndex) % (this.slides.length);
+    this.currentSliderName = this.slides[this.sliderIndex].name;
+
+  }
 
   scrollToElement(page: string, fragment: string): void {
     const element = document.querySelector(`#${fragment}`)
@@ -83,5 +98,6 @@ export class LimitedComponent implements OnInit {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' })
     }
   }
+
 
 }
