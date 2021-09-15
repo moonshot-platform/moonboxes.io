ca0625810346c777f92975c1041ab56bcad814e4
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-09-15 17:14:26 +0300

    added jump to attributes functionality  in inventory page

--- src/app/components/moonbase/inventory/inventory.component.ts
+++ src/app/components/moonbase/inventory/inventory.component.ts
@@ -8,204 +8,186 @@ import { SocialShareComponent } from '../modal-for-transaction/social-share/soci
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss','./../moonbase.component.scss', './../intro/intro.component.scss']
+  styleUrls: ['./inventory.component.scss', './../moonbase.component.scss', './../intro/intro.component.scss']
 })
 export class InventoryComponent implements OnInit {
   static readonly routeName: string = 'inventory';
   data: any;
-  isConnected:boolean = false;
+  isConnected: boolean = false;
   inventoryList: any;
-  
+
   p: number = 1;
-  maxSize:number = 9;
+  maxSize: number = 9;
   inventoryListUpcoming: any;
   lootBoxDetails = [];
   lootBoxDetailsAttributes = [];
   lootBoxDetailsAttributesMobile = [];
   isNSFWStatus = false;
- 
-  constructor(private walletConnectService:WalletConnectService,
-    private httpApi:HttpApiService,private toastrService:ToastrService,
-    public dialog: MatDialog) { 
-      this.lootBoxDetails = httpApi.lootBoxDetails;
-    }
+
+  constructor(private walletConnectService: WalletConnectService,
+    private httpApi: HttpApiService, private toastrService: ToastrService,
+    public dialog: MatDialog) {
+    this.lootBoxDetails = httpApi.lootBoxDetails;
+  }
 
   ngOnInit(): void {
     this.walletConnectService.init();
     this.isNSFWStatus = this.httpApi.getNSFWStatus();
     this.checkNSFWStatus();
-    setTimeout(async() => {
-      await this.walletConnectService.getData().subscribe((data)=>{
-        this.data=data;
+    setTimeout(async () => {
+      await this.walletConnectService.getData().subscribe((data) => {
+        this.data = data;
       });
 
-      if(this.data!==undefined && this.data.address!=undefined)
-      {
-        this.isConnected=true;
+      if (this.data !== undefined && this.data.address != undefined) {
+        this.isConnected = true;
         this.getUserData();
       }
-      else
-      {
+      else {
         this.isConnected = false;
       }
     }, 1000);
   }
 
-  getUserData()
-  {
+  getUserData() {
     this.httpApi.getUserInventory({
-      userAddress : this.data.address,
-      nsfwstatus : this.isNSFWStatus
-    }).subscribe((response:any)=>{
-      if(response.isSuccess){
-        this.inventoryList=response.data.data;
+      userAddress: this.data.address,
+      nsfwstatus: this.isNSFWStatus
+    }).subscribe((response: any) => {
+      if (response.isSuccess) {
+        this.inventoryList = response.data.data;
       }
-      else
-      {
+      else {
         this.toastrService.error("something went wrong");
       }
     });
 
     this.httpApi.getuserUpcomingNft({
-      userAddress : this.data.address,
-      nsfw : this.isNSFWStatus
-    }).subscribe((response:any)=>{
-      if(response.isSuccess){
-        this.inventoryListUpcoming=response.data.data;
-        
+      userAddress: this.data.address,
+      nsfw: this.isNSFWStatus
+    }).subscribe((response: any) => {
+      if (response.isSuccess) {
+        this.inventoryListUpcoming = response.data.data;
+
       }
-      else
-      {
+      else {
         this.toastrService.error("something went wrong");
       }
     });
   }
 
-  
 
-  getImagePath(type)
-  {
-      if(type=="Wood")
-      {
-        return this.lootBoxDetails[0].img;
-      }
-      else if(type=="Silver")
-      {
-        return this.lootBoxDetails[1].img;
-      }
-      else if(type=="Gold")
-      {
-        return this.lootBoxDetails[2].img;
-      }
-      else 
-      {
-        return this.lootBoxDetails[3].img;
-      }
+
+  getImagePath(type) {
+    if (type == "Wood") {
+      return this.lootBoxDetails[0].img;
+    }
+    else if (type == "Silver") {
+      return this.lootBoxDetails[1].img;
+    }
+    else if (type == "Gold") {
+      return this.lootBoxDetails[2].img;
+    }
+    else {
+      return this.lootBoxDetails[3].img;
+    }
   }
 
-  setSelected(index:number,item:any)
-  {
-    this.lootBoxDetailsAttributes=[];
-    this.lootBoxDetailsAttributes[index]=item;
+  setSelected(index: number, item: any) {
+    this.lootBoxDetailsAttributes = [];
+    this.lootBoxDetailsAttributes[index] = item;
     this.lootBoxDetailsAttributes[index].disabled = false;
+    setTimeout(() => {
+      this.scrollToElement('', 'attribute-info');
+    }, 100);
   }
 
-  setSelectedMobile(index:number,item:any)
-  {
-    this.lootBoxDetailsAttributesMobile =[];
-    this.lootBoxDetailsAttributesMobile[index]=item;
+  setSelectedMobile(index: number, item: any) {
+    this.lootBoxDetailsAttributesMobile = [];
+    this.lootBoxDetailsAttributesMobile[index] = item;
     this.lootBoxDetailsAttributesMobile[index].disabled = false;
+    setTimeout(() => {
+      this.scrollToElement('', 'attribute-info-mobile');
+    }, 100);
   }
 
-  closeAttributes()
-  {
-    this.lootBoxDetailsAttributes=[];
-    this.lootBoxDetailsAttributesMobile=[];
+  closeAttributes() {
+    this.lootBoxDetailsAttributes = [];
+    this.lootBoxDetailsAttributesMobile = [];
   }
 
-  claimReward(details:any,event:any,index:any)
-  {
+  claimReward(details: any, event: any, index: any) {
     this.lootBoxDetailsAttributes[index].disabled = true;
-    
+
     this.httpApi.claimRewardDetails({
-      userAddress : this.data.address,
-      nftId : details.nftId
-    }).subscribe((response)=>{
-      if(response.isSuccess)
-      {
-        this.claimRewardTransaction(response.data,details.nftId,details.total,index);
+      userAddress: this.data.address,
+      nftId: details.nftId
+    }).subscribe((response) => {
+      if (response.isSuccess) {
+        this.claimRewardTransaction(response.data, details.nftId, details.total, index);
       }
-      else{
+      else {
         this.lootBoxDetailsAttributes[index].disabled = false;
       }
 
     })
-  
+
   }
 
-  async claimRewardTransaction(data:any,nftId,supply:Number,index:any)
-  {
-    try{
+  async claimRewardTransaction(data: any, nftId, supply: Number, index: any) {
+    try {
       debugger
-      var txnstatus:any = await this.walletConnectService.claimRewardTransaction(
-        data.junkAmount,nftId,supply,data.id,data.id,data.signHash
+      var txnstatus: any = await this.walletConnectService.claimRewardTransaction(
+        data.junkAmount, nftId, supply, data.id, data.id, data.signHash
       );
 
-      if(txnstatus.status)
-      {
+      if (txnstatus.status) {
         txnstatus.hash.wait(1);
-            this.httpApi.claimRewardTransactionHashUpdate({
-              userAddress:this.data.address,
-              id:data.id,
-              transactionHash:txnstatus.hash.hash
-            }).subscribe((response:any)=>
-            {
-                if(response.isSuccess)
-                {
-                  this.lootBoxDetailsAttributes[index].isRewardAvailable = false;
-                  this.httpApi.showToastr(response.data.message,true);
-                }
-                else{
-                  this.lootBoxDetailsAttributes[index].disabled = false;
-                  this.httpApi.showToastr(response.data.message,false);
-                }
-                debugger
-            })
+        this.httpApi.claimRewardTransactionHashUpdate({
+          userAddress: this.data.address,
+          id: data.id,
+          transactionHash: txnstatus.hash.hash
+        }).subscribe((response: any) => {
+          if (response.isSuccess) {
+            this.lootBoxDetailsAttributes[index].isRewardAvailable = false;
+            this.httpApi.showToastr(response.data.message, true);
+          }
+          else {
+            this.lootBoxDetailsAttributes[index].disabled = false;
+            this.httpApi.showToastr(response.data.message, false);
+          }
+          debugger
+        })
       }
     }
-    catch(e)
-    {
+    catch (e) {
       this.lootBoxDetailsAttributes[index].disabled = false;
     }
   }
 
 
-  checkNSFWStatus()
-  {
+  checkNSFWStatus() {
     setInterval(() => {
       this.checkNSFWStatusFromStorage()
     }, 4000);
-    
+
   }
 
-  checkNSFWStatusFromStorage()
-  {
+  checkNSFWStatusFromStorage() {
     let tempstatus = this.httpApi.getNSFWStatus();
-      if(this.isNSFWStatus!=tempstatus)
-      {
-        this.isNSFWStatus = tempstatus;
-        this.getUserData();
-      }
+    if (this.isNSFWStatus != tempstatus) {
+      this.isNSFWStatus = tempstatus;
+      this.getUserData();
+    }
   }
 
-  checkFileType(url:string)
-  {
-    const images = ["jpg", "gif", "png","jpeg", "JPG", "GIF", "PNG", "JPEG"]
+  checkFileType(url: string) {
+    const images = ["jpg", "gif", "png", "jpeg", "JPG", "GIF", "PNG", "JPEG"]
     const videos = ["mp4", "3gp", "ogg"]
 
     const urltemp = new URL(url)
     const extension = urltemp.pathname.substring(urltemp.pathname.lastIndexOf('.') + 1)
-    
+
     if (images.includes(extension)) {
       return "true"
     } else if (videos.includes(extension)) {
@@ -214,11 +196,18 @@ export class InventoryComponent implements OnInit {
     return false;
   }
 
-  openDialog(imagePath:string) {
+  openDialog(imagePath: string) {
     let dialogRef = this.dialog.open(SocialShareComponent, {
       width: 'auto',
-       data: { name: imagePath }
+      data: { name: imagePath }
     });
-  }  
+  }
+
+  scrollToElement(page: string, fragment: string): void {
+    const element = document.querySelector(`#${fragment}`)
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }
 
 }
