9b2305ff16af902c683d4603717142bd03d846ad
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-09-15 13:56:04 +0300

    added scroll jump to collection info section

--- src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
+++ src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
@@ -5,14 +5,14 @@ import { HttpApiService } from 'src/app/services/http-api.service';
 @Component({
   selector: 'app-upcoming',
   templateUrl: './upcoming.component.html',
-  styleUrls: ['./upcoming.component.scss','./../../buy-moonbase/buy-moonbase.component.scss','./../../moonbase.component.scss', './../../intro/intro.component.scss']
+  styleUrls: ['./upcoming.component.scss', './../../buy-moonbase/buy-moonbase.component.scss', './../../moonbase.component.scss', './../../intro/intro.component.scss']
 })
 export class UpcomingComponent implements OnInit {
   static readonly routeName: string = 'upcoming';
   listOfArtistCollection = [];
   listOfArtistUpcoming = [];
   listOfRecentDrops = [];
-  endTime  = '2021-09-29T00:00:00';
+  endTime = '2021-09-29T00:00:00';
   isNSFWStatus = false;
   activeTab = 1;
   lootBoxDetailsAttributes = [];
@@ -22,8 +22,8 @@ export class UpcomingComponent implements OnInit {
   public innerWidth: any;
   singleView = 4;
 
-  constructor(private httpService:HttpApiService) { 
-    
+  constructor(private httpService: HttpApiService) {
+
   }
 
   ngOnInit(): void {
@@ -31,85 +31,84 @@ export class UpcomingComponent implements OnInit {
     this.checkNSFWStatus();
     this.getAllCollections();
 
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
       this.singleView = 1;
-    }    
+    }
   }
 
-  changeTab(tabIndex:number)
-  {
+  changeTab(tabIndex: number) {
     this.closeAttributes();
     this.activeTab = tabIndex;
-    if(this.activeTab==1){
+    if (this.activeTab == 1) {
       this.listData = this.listOfArtistUpcoming;
     }
-    else if(this.activeTab==2){
+    else if (this.activeTab == 2) {
       this.listData = this.listOfArtistCollection;
     }
-    else if(this.activeTab==3){
+    else if (this.activeTab == 3) {
       this.listData = this.listOfRecentDrops;
     }
   }
 
-  getAllCollections()
-  {
-    
-    this.httpService.getAllCollections(this.isNSFWStatus).subscribe((response)=>{
-      
-      this.listOfArtistCollection=response.data.live_data_array;
-        
+  getAllCollections() {
+
+    this.httpService.getAllCollections(this.isNSFWStatus).subscribe((response) => {
+
+      this.listOfArtistCollection = response.data.live_data_array;
+
       this.listOfRecentDrops = response.data.recent_data_array;
-        
+
     });
 
-      this.httpService.getUpcomingArtistCollections(this.isNSFWStatus).subscribe((response)=>{
-        this.listOfArtistUpcoming=response.data;
-        this.listData = this.listOfArtistUpcoming;
+    this.httpService.getUpcomingArtistCollections(this.isNSFWStatus).subscribe((response) => {
+      this.listOfArtistUpcoming = response.data;
+      this.listData = this.listOfArtistUpcoming;
     });
   }
 
-  checkNSFWStatus()
-  {
+  checkNSFWStatus() {
     setInterval(() => {
       this.checkNSFWStatusFromStorage()
     }, 4000);
-    
+
   }
 
-  checkNSFWStatusFromStorage()
-  {
+  checkNSFWStatusFromStorage() {
     let tempstatus = this.httpService.getNSFWStatus();
-      if(this.isNSFWStatus!=tempstatus)
-      {
-        this.isNSFWStatus = tempstatus;
-        this.getAllCollections();
-      }
+    if (this.isNSFWStatus != tempstatus) {
+      this.isNSFWStatus = tempstatus;
+      this.getAllCollections();
+    }
   }
-  
- 
 
-  setSelected(index:number,item:any)
-  {
-    this.lootBoxDetailsAttributes=[];
-    this.lootBoxDetailsAttributes[index]=item;
+
+
+  setSelected(index: number, item: any) {
+    this.lootBoxDetailsAttributes = [];
+    this.lootBoxDetailsAttributes[index] = item;
     this.lootBoxDetailsAttributes[index].disabled = false;
+    setTimeout(() => {
+      this.scrollToElement('', 'collection-info');
+    }, 100);
   }
 
-  setSelectedMobile(index:number,item:any)
-  {
-    this.lootBoxDetailsAttributesMobile =[];
-    this.lootBoxDetailsAttributesMobile[index]=item;
+  setSelectedMobile(index: number, item: any) {
+    this.lootBoxDetailsAttributesMobile = [];
+    this.lootBoxDetailsAttributesMobile[index] = item;
     this.lootBoxDetailsAttributesMobile[index].disabled = false;
+    this.scrollToElement('', 'collection-info');
   }
 
-  closeAttributes()
-  {
-    this.lootBoxDetailsAttributes=[];
-    this.lootBoxDetailsAttributesMobile=[];
+  closeAttributes() {
+    this.lootBoxDetailsAttributes = [];
+    this.lootBoxDetailsAttributesMobile = [];
   }
 
- 
-
-  
+  scrollToElement(page: string, fragment: string): void {
+    const element = document.querySelector(`#${fragment}`)
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }
 
 }
