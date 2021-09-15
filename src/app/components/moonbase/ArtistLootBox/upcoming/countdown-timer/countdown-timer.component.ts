4049623cdadb9180c15a3a6a0dcbdbd27ec3a441
S1nPur1ty <singular.purity@gmail.com>
2021-09-15 17:55:59 +0800

    fixed styling related issues in the upcoming pages

--- src/app/components/moonbase/ArtistLootBox/upcoming/countdown-timer/countdown-timer.component.ts
+++ src/app/components/moonbase/ArtistLootBox/upcoming/countdown-timer/countdown-timer.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { formatDate } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core';
+import {
+  formatDate
+} from '@angular/common';
 
 @Component({
   selector: 'app-countdown-timer',
@@ -7,50 +13,49 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./countdown-timer.component.scss']
 })
 export class CountdownTimerComponent implements OnInit {
-  @Input() date:string;
-  interval:any;
-  timer:any=0;
+  @Input() date: string;
+  interval: any;
+  timer: any = 0;
   checkdate = "";
-  constructor() { }
+  constructor() {}
 
-  ngOnDestroy()
-  {
+  ngOnDestroy() {
     clearInterval(this.interval);
   }
 
   ngOnInit(): void {
     // Set the date we're counting down to
- var date =   new Date(this.date + " UTC");
-var countDownDate = new Date(formatDate(date.toString(), "yyyy/MM/dd HH:mm:ss", "en-us")).getTime();
-// Update the count down every 1 second
-this.interval = setInterval(function() {
+    var date = new Date(this.date + " UTC");
+    var countDownDate = new Date(formatDate(date.toString(), "yyyy/MM/dd HH:mm:ss", "en-us")).getTime();
+    // Update the count down every 1 second
+    this.interval = setInterval(function () {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+      // Get today's date and time
+      var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  // Display the result in the element with id="demo"
-  if(days>0 || hours>0 || minutes >0 || seconds>0)
-  {
-    this.timer = days+" D : "+hours+" H : "+minutes+" M : "+seconds+" S";
-  }
-  else
-  {
-    this.timer=0;
-  }
-  
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(this.interval);
-  }
-}.bind(this), 1000);
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      // Display the result in the element with id="demo"
+      if (days > 0 || hours > 0 || minutes > 0 || seconds > 0) {
+        this.timer = (days <= 9 ? `0${days}` : days) + " D : " + 
+        (hours <= 9 ? `0${hours}` : hours) + " H : " + 
+        (minutes <= 9 ? `0${minutes}` : minutes)  + " M : " + 
+        (seconds <= 9 ? `0${seconds}` : seconds)  + " S";
+      } else {
+        this.timer = 0;
+      }
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(this.interval);
+      }
+    }.bind(this), 1000);
   }
 
 }
--- src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
+++ src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
@@ -5,53 +5,111 @@ import { HttpApiService } from 'src/app/services/http-api.service';
 @Component({
   selector: 'app-upcoming',
   templateUrl: './upcoming.component.html',
-  styleUrls: ['./upcoming.component.scss', './../../buy-moonbase/buy-moonbase.component.scss', './../../moonbase.component.scss', './../../intro/intro.component.scss']
+  styleUrls: ['./upcoming.component.scss','./../../buy-moonbase/buy-moonbase.component.scss','./../../moonbase.component.scss', './../../intro/intro.component.scss']
 })
 export class UpcomingComponent implements OnInit {
   static readonly routeName: string = 'upcoming';
   listOfArtistCollection = [];
   listOfArtistUpcoming = [];
   listOfRecentDrops = [];
-  endTime = '2021-09-29T00:00:00';
+  endTime  = '2021-09-29T00:00:00';
   isNSFWStatus = false;
+  activeTab = 1;
+  lootBoxDetailsAttributes = [];
+  lootBoxDetailsAttributesMobile = [];
+  artistDetails: any;
+  listData = [];
+  public innerWidth: any;
+  singleView = 4;
 
-  constructor(private httpService: HttpApiService) { }
+  constructor(private httpService:HttpApiService) { 
+    
+  }
 
   ngOnInit(): void {
     this.isNSFWStatus = this.httpService.getNSFWStatus();
     this.checkNSFWStatus();
     this.getAllCollections();
 
+    if(window.innerWidth < 768) {
+      this.singleView = 1;
+    }    
   }
 
-  getAllCollections() {
-    this.httpService.getAllCollections(this.isNSFWStatus).subscribe((response) => {
-      this.listOfArtistCollection = response.data.live_data_array;
-      this.listOfRecentDrops = response.data.recent_data_array;
-
-    });
-
-    this.httpService.getUpcomingArtistCollections(this.isNSFWStatus).subscribe((response) => {
-
-      this.listOfArtistUpcoming = response.data;
-      //this.listOfArtistUpcoming.push(response.data[0]);
-    });
-  }
-
-  checkNSFWStatus() {
-    setInterval(() => {
-      this.checkNSFWStatusFromStorage()
-    }, 4000);
-
-  }
-
-  checkNSFWStatusFromStorage() {
-    let tempstatus = this.httpService.getNSFWStatus();
-    if (this.isNSFWStatus != tempstatus) {
-      this.isNSFWStatus = tempstatus;
-      this.getAllCollections();
+  changeTab(tabIndex:number)
+  {
+    this.closeAttributes();
+    this.activeTab = tabIndex;
+    if(this.activeTab==1){
+      this.listData = this.listOfArtistUpcoming;
+    }
+    else if(this.activeTab==2){
+      this.listData = this.listOfArtistCollection;
+    }
+    else if(this.activeTab==3){
+      this.listData = this.listOfRecentDrops;
     }
   }
 
+  getAllCollections()
+  {
+    
+    this.httpService.getAllCollections(this.isNSFWStatus).subscribe((response)=>{
+      
+      this.listOfArtistCollection=response.data.live_data_array;
+        
+      this.listOfRecentDrops = response.data.recent_data_array;
+        
+    });
+
+      this.httpService.getUpcomingArtistCollections(this.isNSFWStatus).subscribe((response)=>{
+        this.listOfArtistUpcoming=response.data;
+        this.listData = this.listOfArtistUpcoming;
+    });
+  }
+
+  checkNSFWStatus()
+  {
+    setInterval(() => {
+      this.checkNSFWStatusFromStorage()
+    }, 4000);
+    
+  }
+
+  checkNSFWStatusFromStorage()
+  {
+    let tempstatus = this.httpService.getNSFWStatus();
+      if(this.isNSFWStatus!=tempstatus)
+      {
+        this.isNSFWStatus = tempstatus;
+        this.getAllCollections();
+      }
+  }
+  
+ 
+
+  setSelected(index:number,item:any)
+  {
+    this.lootBoxDetailsAttributes=[];
+    this.lootBoxDetailsAttributes[index]=item;
+    this.lootBoxDetailsAttributes[index].disabled = false;
+  }
+
+  setSelectedMobile(index:number,item:any)
+  {
+    this.lootBoxDetailsAttributesMobile =[];
+    this.lootBoxDetailsAttributesMobile[index]=item;
+    this.lootBoxDetailsAttributesMobile[index].disabled = false;
+  }
+
+  closeAttributes()
+  {
+    this.lootBoxDetailsAttributes=[];
+    this.lootBoxDetailsAttributesMobile=[];
+  }
+
+ 
+
+  
 
 }
