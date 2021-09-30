694b2169e1580047558a5909d47e4b5700c1808b
Moonshot <>
2021-09-30 23:44:05 +0200

    tab order is 3,2,1 instead of 1,2,3

--- src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
+++ src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
@@ -15,7 +15,7 @@ export class UpcomingComponent implements OnInit {
   listOfRecentDrops = [];
   endTime = '2021-09-29T00:00:00';
   isNSFWStatus = false;
-  activeTab = 3;
+  activeTab = 1;
   lootBoxDetailsAttributes = [];
   lootBoxDetailsAttributesMobile = [];
   artistDetails: any;
