4fc1d2370ce17ec335034a215c000158cd680c9c
S1nPur1ty <singular.purity@gmail.com>
2021-10-10 21:31:24 +0800

    fix for nsfw switch and rabbit collection data for now showing up and removed the unnecessary delay

--- src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
+++ src/app/components/moonbase/ArtistLootBox/upcoming/upcoming.component.ts
@@ -27,8 +27,31 @@ export class UpcomingComponent implements OnInit {
   data: any;
   userAddress: any;
   reveal = false;
-  fakeCollectionData: any;
   public isTooltipActive: boolean[] = [];
+
+  fakeCollectionData = {
+    ArtistName: "Ra8bits",
+    NSFW: false.valueOf,
+    TotalMaxSupply: 1,
+    description: "In an epic journey through outer space, the MoonShooter finds himself on a mission to explore the surface of the Moon. His objective? To continue the unending search for the uncharted relic artifacts. Legend says there are orb-like objects scattered throughout the Moon, which behold powers beyond our perception. Only those, truly balanced by heart and mind, will be able to observe the orbs and harvest their energy.\n\nWill he be the one? The one that will be allowed to witness the true and enormous power of these mysterious orbs?\n\nCheck out the first episode of The Moonshooter \"First Landing\" here:  https://youtu.be/uRi-6OL-QrI",
+    discord: "https://discord.com/invite/QrXVQRKzrh",
+    filePath: "assets/media/icons/drops/The_Ra8bits_Series.jpg",
+    id: 1,
+    instagram: "https://www.instagram.com/moonshotrs25/",
+    isAdmin: true,
+    minPrice: "-",
+    name: "The Ra8bits Series",
+    opensea: "",
+    rarible: "",
+    revealDate: "2021/10/07 18:00:00",
+    supply: 1,
+    telegram: "https://t.me/MoonShotChat",
+    transactionhash: "0x09d7e6b10a34748f2399b146c3ef275f2d2628dad324f7a6a1184d0c963e8463",
+    twitter: "https://twitter.com/RS25Moonshot",
+    walletAddress: "0x4fdf3c15758EFc81Ad809420ABd7bD1D02B4734e",
+    website: "https://project-moonshot.me/",
+    isFake: true,
+  };
 
   constructor(
     private httpService: HttpApiService,
@@ -43,6 +66,7 @@ export class UpcomingComponent implements OnInit {
   ngOnInit(): void {
     this.isNSFWStatus = this.httpService.getNSFWStatus();
     this.checkNSFWStatus();
+    
     this.getConnectedAccount();
 
     if (window.innerWidth < 768) {
@@ -74,7 +98,11 @@ export class UpcomingComponent implements OnInit {
     if (this.activeTab == 1) {
       this.title.setTitle('Moonbox drops - Upcoming');
       this.location.go('/upcoming');
+      
+      this.listData = [];
+      this.listOfArtistUpcoming = []
       this.listData = this.listOfArtistUpcoming;
+      this.listData.push(this.fakeCollectionData);
     }
 
     else if (this.activeTab == 2) {
@@ -96,25 +124,21 @@ export class UpcomingComponent implements OnInit {
 
 
   async getConnectedAccount() {
-    await this.walletConnectService.getData().subscribe((data) => {
+    this.walletConnectService.getData().subscribe((data) => {
       if (this.data != data && data != undefined && data.address != undefined) {
         this.data = data;
         this.userAddress = data.address;
-        // this.userWalletAddress = data.address;
 
         this.getAllCollections();
       }
     });
 
-    setTimeout(() => {
-
-      if (this.data == undefined || this.data.address == undefined) {
-        this.getAllCollections();
-      }
-    }, 2500);
+    if (this.data == undefined || this.data.address == undefined) {
+      this.getAllCollections();
+    }
   }
-  async getAllCollections() {
 
+  async getAllCollections() {
 
     this.httpService.getAllCollections(this.isNSFWStatus, this.userAddress).subscribe((response) => {
 
@@ -131,34 +155,12 @@ export class UpcomingComponent implements OnInit {
     });
 
     this.httpService.getUpcomingArtistCollections(this.isNSFWStatus, this.userAddress).subscribe((response) => {
-      this.listOfArtistUpcoming = response.data;
-      this.fakeCollectionData = {
-        ArtistName: "Ra8bits",
-        NSFW: false.valueOf,
-        TotalMaxSupply: 1,
-        description: "In an epic journey through outer space, the MoonShooter finds himself on a mission to explore the surface of the Moon. His objective? To continue the unending search for the uncharted relic artifacts. Legend says there are orb-like objects scattered throughout the Moon, which behold powers beyond our perception. Only those, truly balanced by heart and mind, will be able to observe the orbs and harvest their energy.\n\nWill he be the one? The one that will be allowed to witness the true and enormous power of these mysterious orbs?\n\nCheck out the first episode of The Moonshooter \"First Landing\" here:  https://youtu.be/uRi-6OL-QrI",
-        discord: "https://discord.com/invite/QrXVQRKzrh",
-        filePath: "assets/media/icons/drops/The_Ra8bits_Series.jpg",
-        id: 1,
-        instagram: "https://www.instagram.com/moonshotrs25/",
-        isAdmin: true,
-        minPrice: "-",
-        name: "The Ra8bits Series",
-        opensea: "",
-        rarible: "",
-        revealDate: "2021/10/07 18:00:00",
-        supply: 1,
-        telegram: "https://t.me/MoonShotChat",
-        transactionhash: "0x09d7e6b10a34748f2399b146c3ef275f2d2628dad324f7a6a1184d0c963e8463",
-        twitter: "https://twitter.com/RS25Moonshot",
-        walletAddress: "0x4fdf3c15758EFc81Ad809420ABd7bD1D02B4734e",
-        website: "https://project-moonshot.me/",
-        isFake: true,
-      };
-      if (this.activeTab == 1) {
-        this.listData = this.listOfArtistUpcoming;
-        console.log(this.listData[0]);
 
+      this.listOfArtistUpcoming = response.data;
+
+      if (this.activeTab == 1) {
+        this.listData = [];
+        this.listData = this.listOfArtistUpcoming;
         this.listData.push(this.fakeCollectionData);
       }
       for (let index = 0; index < this.listData.length; index++) {
