a3cc2328dd2b193ffacc7a78445626408ab2dbd8
S1nPur1ty <singular.purity@gmail.com>
2021-12-03 22:18:19 +0800

    upon clicking on view details for both drops and inventory sections, a popup shows up instead of a field that is all the way below the same content where all the items are listed. more updates will follow soon for smaller screen sizes.

---
+++ src/app/components/base/dialogs/collection-overview/collection-overview.component.ts
@@ -0,0 +1,32 @@
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-overview',
+  templateUrl: './collection-overview.component.html',
+  styleUrls: ['./collection-overview.component.scss']
+})
+export class CollectionOverviewComponent implements OnInit {
+
+  item: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.item = data;
+    console.log(data);
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  getMinPrice( item: any ) {
+    const { Diamond, Wood, Silver, Gold } = item;
+    if( Diamond == Wood && Diamond == Silver && Diamond && Gold  )
+      return item['minPrice'];
+
+    return `from ${item['minPrice']}`;
+  }
+
+}
