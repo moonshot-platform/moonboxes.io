896e1d3514be761df08e681ebfe6b62ffe2e4c14
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-01-12 19:26:44 +0300

    Add slider model

--- src/app/components/moonbase/landing/consts/nft-slider.const.ts
+++ src/app/components/moonbase/landing/consts/nft-slider.const.ts
@@ -1,167 +1,168 @@
-import { SliderModel } from "../models/slider.model";
+import { LandingSliderModel } from "src/app/models/landing-slider.model";
 
-export const nftSlider: SliderModel[] = [
-    new SliderModel(
+
+export const nftSlider: LandingSliderModel[] = [
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-06271.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02374.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/322.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-06108.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02384.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/950.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/sharpei/236.webp",
         "Happy SharPei Yacht Club",
         "artist/0x1Aa800bE7e9829DDF97C54EF750F5E42C2a6a884",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-06106.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/fractal-art/Da-Vinci-Mona-Lisa.webp",
         "Fractal Art",
         "artist/0x24B5BF0aE8F4EeC63549B413F3De903C64B2e101",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02438.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/951.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/sharpei/170.webp",
         "Happy SharPei Yacht Club",
         "artist/0x1Aa800bE7e9829DDF97C54EF750F5E42C2a6a884",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-06071.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/fractal-art/Hokusai-The-Great-Wave-off-Kanagawa.webp",
         "Fractal Art",
         "artist/0x24B5BF0aE8F4EeC63549B413F3De903C64B2e101",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02478.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/068.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/sharpei/40.webp",
         "Happy SharPei Yacht Club",
         "artist/0x1Aa800bE7e9829DDF97C54EF750F5E42C2a6a884",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-06045.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/fractal-art/Liechtenstein-Girl-with-Hair-Ribbon.webp",
         "Fractal Art",
         "artist/0x24B5BF0aE8F4EeC63549B413F3De903C64B2e101",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02532.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/372.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/sharpei/15.webp",
         "Happy SharPei Yacht Club",
         "artist/0x1Aa800bE7e9829DDF97C54EF750F5E42C2a6a884",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-05925.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/kallies/5-Princess-Kallie.webp",
         "Kallies NFT World",
         "artist/0x4Bde39352B2204C653F24c611C7C7db5Cc5C1070"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/fractal-art/Vermeer-Girl-with-a-Pearl-Earring.webp",
         "Fractal Art",
         "artist/0x24B5BF0aE8F4EeC63549B413F3De903C64B2e101",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02550.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/741.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/sharpei/1.webp",
         "Happy SharPei Yacht Club",
         "artist/0x1Aa800bE7e9829DDF97C54EF750F5E42C2a6a884",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/males/character-05918.webp",
         "The Ra8bitsNFT Series",
         "artist/0x4b656fAB214D2e493534764E4074E2881A7A5f16",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/kallies/8-Party-Kitties.webp",
         "Kallies NFT World",
         "artist/0x4Bde39352B2204C653F24c611C7C7db5Cc5C1070"
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/fractal-art/Warhol-Marilyn-Monroe.webp",
         "Fractal Art",
         "artist/0x24B5BF0aE8F4EeC63549B413F3De903C64B2e101",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/females/character-02559.webp",
         "The Female Ra8bits Series",
         "artist/0x522C8228f61f6102F0b8bC138ea2E00D616811Ee",
     ),
-    new SliderModel(
+    new LandingSliderModel(
         "assets/media/images/moonbox/landing/nfts/moonshooter/575.webp",
         "The Moonshooter Series",
         "artist/0x66A7B9f608378e59105022aB00b0F541666e8c4d"
