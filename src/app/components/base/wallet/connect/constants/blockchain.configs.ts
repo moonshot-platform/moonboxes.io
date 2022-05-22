02d51cc2e0a00b0bf447c9282bea3850bb2664b9
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-05-22 20:12:02 +0300

    Add background color attribute to blockchain confs

--- src/app/components/base/wallet/connect/constants/blockchain.configs.ts
+++ src/app/components/base/wallet/connect/constants/blockchain.configs.ts
@@ -1,15 +1,18 @@
 export const CHAIN_CONFIGS: any = {
   "1": {
     "name": "Ethereum Mainnet",
-    "icon": "assets/media/images/blockchain/eth.webp"
+    "icon": "assets/media/images/blockchain/eth.webp",
+    "bg": "black",
   },
   "1285": {
     "name": "Moonriver Mainnet",
-    "icon": "assets/media/images/blockchain/MOVR.webp"
+    "icon": "assets/media/images/blockchain/MOVR.webp",
+    "bg": "#6e56a4",
   },
   "97": {
     "name": "BSC Testnet",
     "icon": "assets/media/images/blockchain/BSC.svg",
+    "bg": "black",
     "config": {
       method: 'wallet_addEthereumChain',
       params: [{
@@ -26,15 +29,18 @@ export const CHAIN_CONFIGS: any = {
   },
   "56": {
     "name": "BSC Mainnet",
-    "icon": "assets/media/images/blockchain/BSC.svg"
+    "icon": "assets/media/images/blockchain/BSC.svg",
+    "bg": "black",
   },
   "137": {
     "name": "Polygon Mainnet",
-    "icon": "assets/media/images/blockchain/polygon.svg"
+    "icon": "assets/media/images/blockchain/polygon.svg",
+    "bg": "#6e56a4",
   },
   "1287": {
     "name": "Moonbase Alpha",
     "icon": "assets/media/images/blockchain/MOVR.webp",
+    "bg": "black",
     "config": {
       method: 'wallet_addEthereumChain',
       params: [{
@@ -52,6 +58,7 @@ export const CHAIN_CONFIGS: any = {
   "80001": {
     "name": "Mumbai Testnet",
     "icon": "assets/media/images/blockchain/mumbai.webp",
+    "bg": "white",
     "config": {
       method: 'wallet_addEthereumChain',
       params: [{
